fae460517938c4cf49c87e59882d8d0e682b738c
Serge Gorbachev <sng78@list.ru>
2025-01-23 16:19:30 +0300

    add models

---
+++ src/main/java/io/github/sng78/models/Person.java
@@ -0,0 +1,43 @@
+package io.github.sng78.models;
+
+public class Person {
+
+    private int id;
+
+    private String fullName;
+
+    private int yearOfBirth;
+
+    public Person() {
+    }
+
+    public Person(int id, String fullName, int yearOfBirth) {
+        this.id = id;
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
