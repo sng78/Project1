40c3bff855169ced20e554042986d208dd96d4c8
Serge Gorbachev <sng78@list.ru>
2025-01-30 10:44:54 +0300

    add show person info & create new person, fix initDb

--- src/main/java/io/github/sng78/controllers/PersonController.java
+++ src/main/java/io/github/sng78/controllers/PersonController.java
@@ -1,10 +1,10 @@
 package io.github.sng78.controllers;
 
 import io.github.sng78.dao.PersonDao;
+import io.github.sng78.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -12,7 +12,7 @@ public class PersonController {
 
     private final PersonDao personDao;
 
-    public PersonController(final PersonDao personDao) {
+    public PersonController(PersonDao personDao) {
         this.personDao = personDao;
     }
 
@@ -20,5 +20,22 @@ public class PersonController {
     public String index(Model model) {
         model.addAttribute("people", personDao.findAll());
         return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.findById(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/io/github/sng78/dao/PersonDao.java
+++ src/main/java/io/github/sng78/dao/PersonDao.java
@@ -21,4 +21,15 @@ public class PersonDao {
     public List<Person> findAll() {
         return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
     }
+
+    public Person findById(int id) {
+        return jdbcTemplate.query("SELECT * FROM person WHERE id=?",
+                        new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO person(full_name, year_of_birth) VALUES(?, ?)",
+                person.getFullName(), person.getYearOfBirth());
+    }
 }
